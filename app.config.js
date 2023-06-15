64223017e3bdf90d71c35e29ebb9b7f8185a6c77
Haader <37Hader38@gmail.com>
2023-06-15 18:07:52 -0300

    app terminada

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+    name: "apdapp",
+    version: "2.6.0",
+    extra: {
+      eas: {
+        projectId: "8d9fa7e4-8b4c-4b45-a449-da3d745316b5"
+      }
+    },
+    // ... otras configuraciones ...
+  };
