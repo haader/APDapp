600929978792aca54be0997aa6eb7eb7efb70180
Haader <37Hader38@gmail.com>
2023-06-15 18:16:16 -0300

    cambiando version

--- app.config.js
+++ app.config.js
@@ -3,7 +3,9 @@ export default {
     version: "2.6.0",
     extra: {
       eas: {
-        projectId: "8d9fa7e4-8b4c-4b45-a449-da3d745316b5"
+        projectId: "8d9fa7e4-8b4c-4b45-a449-da3d745316b5",
+        name: "apdapp",
+        version: "2.6.0",
       }
     },
     // ... otras configuraciones ...
