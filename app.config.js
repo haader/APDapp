312e508322692033bbf7362129988a693cb1d9da
Haader <37Hader38@gmail.com>
2023-06-16 01:50:11 -0300

    new changes

--- app.config.js
+++ app.config.js
@@ -1,12 +1,43 @@
-export default {
-    name: "apdapp",
-    version: "2.6.0",
-    extra: {
-      eas: {
-        projectId: "8d9fa7e4-8b4c-4b45-a449-da3d745316b5",
-        name: "apdapp",
-        version: "2.6.0",
+// const myValue = 'APDAPP2';
+
+// module.exports = {
+//   name: myValue,
+//   version:'2.0.0',
+//   projectId: "1d7b4e2b-508d-4384-8a09-6f2e672a813a"
+//   // All values in extra will be passed to your app.
+//   // extra: {
+//   //   fact: 'kittens are cool',
+//   // },
+// };
+
+export default{
+  
+    "expo": {
+      "owner": "37haader38",
+      "slug": "apdapp",
+      "version": '3.0.1',
+      "versionCode": "3",
+      "versionName": "version22",
+      "extra": {
+        "eas": {
+          "projectId": "d6b8be8c-8843-4b43-bad5-e84b99ad3cb5",
+          "version": "3.0"
+        }
       }
-    },
-    // ... otras configuraciones ...
-  };
+    }
+  
+  
+}
+// export default {
+//     name: "apdapp",
+//     version: "2.6.0",
+//     extra: {
+//       eas: {
+//         projectId: "8d9fa7e4-8b4c-4b45-a449-da3d745316b5",
+//         name: "apdapp",
+//         version: "2.6.0",
+//       }
+//     },
+//     // ... otras configuraciones ...
+//   };
+
